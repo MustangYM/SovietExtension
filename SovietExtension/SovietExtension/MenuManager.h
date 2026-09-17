363b162e1a1aec78e4d21145d3f0077d4535ffd0
MustangYM <MustangYM@yeah.net>
2026-09-17 22:35:52 +0800

    1.4.1

--- SovietExtension/SovietExtension/MenuManager.h
+++ SovietExtension/SovietExtension/MenuManager.h
@@ -23,7 +23,7 @@ static NSString *kExitChatroomNick = @"kExitChatroomNick.SOVIET";
 static NSString *kUseSystemWeb = @"kUseSystemWeb.SOVIET";
 static NSString *kIsFirstLoad = @"kIsFirstLoad.SOVIET";
 static NSString *kAutoLogin = @"kAutoLogin.SOVIET";
-static NSString *kCurrentVersion = @"1.4.0";
+static NSString *kCurrentVersion = @"1.4.1";
 
 @interface MenuManager : NSObject
 @property (nonatomic, assign) BOOL hasLoadMistyHook;
